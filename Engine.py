596a8cfde5689a8cbd7fd0314ff59644befcdfa1
isikus <ivantor@yandex.ru>
2018-05-28 10:27:44 +0300

    Corrected methods in route(/)

--- Engine.py
+++ Engine.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
 
-@app.route('/', methods=['GET', 'HEAD'])
+@app.route('/')
 def index():
     return 'ok'
