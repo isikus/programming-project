b144eb1f374793f8e69f2a2ac8e8ed71a9d268d8
isikus <ivantor@yandex.ru>
2018-05-28 10:21:58 +0300

    Added main page GET handler

--- Engine.py
+++ Engine.py
@@ -1,7 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
 
 
 if __name__ == '__main__':
-    import os
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	import os
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'ok'
